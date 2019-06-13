bb5b218898ce2f844e1f88a4f032aee60f5465f0
JJ Ying <yingjunjiu@gmail.com>
2019-06-13 17:31:17 +0800

    更新文件位置

--- assets/main.js
+++ assets/main.js
@@ -1,31 +1,68 @@
 var m = new Vue({
   el: '.wrapper',
   data: {
-    commentSlide: 1,
+    stage: 1,
+    windowWidth: 1200,
+    windowHeight: 700,
     headerScaleRatio: 1,
+    headerFinalScale: 4.8,
+    headerScrollDistance: 500,
     windowHeight: 1000,
-    tvXOffset: 100
+    lastScroll: 0,
+    scrollDirection: 1,
+    tvOffsetX: 0,
+    tvFinalOffsetX: -92,
+    tvOffsetY: 0,
+    tvFinalOffsetY: -24
   },
   methods: {
     checkScroll: function(y){
-      console.log(y)
-      if (this.headerScaleRatio < 3.25) {
-        this.headerScaleRatio = 1 + y/500
+      if (y > this.lastScroll) this.scrollDirection = 1
+      else this.scrollDirection = -1
+
+      if (y > this.headerScrollDistance) {
+        this.stage = 2
+      }
+      else {
+        this.stage = 1
       }
 
-      console.log(this.headerScaleRatio)
+      if (y < this.headerScrollDistance) {
+        this.headerScaleRatio = 1+ y / this.headerScrollDistance * (this.headerFinalScale - 1)
+        this.tvOffsetX = y / this.headerScrollDistance * this.tvFinalOffsetX
+        this.tvOffsetY = y / this.headerScrollDistance * this.tvFinalOffsetY
+      }
+      else {
+        this.headerScaleRatio = this.headerFinalScale
+        this.tvOffsetX = this.tvFinalOffsetX
+        this.tvOffsetY = this.tvFinalOffsetY
+        document.getElementByID('video-op').play()
+      }
+
+      this.lastScroll = y
     },
     diamond: function(n) {
       console.log(n)
-      // n = n / a.maxDiamondSize
       return n / 20
     }
 
   }
 })
 
+var smallScreenW = 379
+var smallScreenH = 283
+var smallScreenHPercent = 0.3
+
 function getSize(){
-  console.log("lalal")
+  m.windowWidth = window.innerWidth
+  m.windowHeight = window.innerHeight
+  if (window.innerWidth>window.innerHeight) {
+    m.headerFinalScale = window.innerWidth / ( window.innerHeight * smallScreenHPercent / smallScreenH * smallScreenW)
+  }
+  else {
+    m.headerFinalScale = 1 / smallScreenHPercent
+  }
+  console.log(m.headerFinalScale)
 }
 
 window.onload = function(){
@@ -35,15 +72,7 @@ window.onload = function(){
 window.onresize = function(){
   getSize()
   console.log("★")
-  var corners = document.querySelectorAll(".x-corner")
-	if (window.innerHeight > heightBreakPoint) {
-    corners[0].classList.add("show")
-    corners[1].classList.add("show")
-	}
-	else {
-    corners[0].classList.remove("show")
-    corners[1].classList.remove("show")
-	}
+  // var corners = document.querySelectorAll(".x-corner")
 }
 
 window.onscroll = function(){
