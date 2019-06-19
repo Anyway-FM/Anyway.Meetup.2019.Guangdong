e8d446c3b7a95d81aef8c2e83549394eb1aad59b
JJ Ying <yingjunjiu@gmail.com>
2019-06-19 23:00:34 +0800

    初步尝试了移动端适配

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,12 @@
+var bgm = document.getElementById('bgm')
 var m = new Vue({
   el: '.wrapper',
   data: {
     stage: 1,
     scrollY: 0,
+    landscape: true,
+    playingMusic: false,
+    muted: false,
     windowWidth: 1200,
     windowHeight: 700,
     headerScaleRatio: 1,
@@ -16,7 +20,8 @@ var m = new Vue({
     lastScroll: 0,
     scrollDirection: 1,
     tvOffsetX: 0,
-    tvFinalOffsetX: -176,
+    tvInitOffsetX: 200,
+    tvFinalOffsetX: 176,
     tvOffsetY: 0,
     tvFinalOffsetY: -60,
     opBgNum: 0,
@@ -25,6 +30,29 @@ var m = new Vue({
     mainScrollGap: 200,
     mainScroll:0,
     bgColorList: ['7c0808','8a9eae','b96605']
+  },
+  watch: {
+    scrollY: function () {
+      if (this.stage == 1) {
+        if (this.landscape) {
+          // this.tvOffsetX = this.windowWidth / 2 +this.tvOffsetX
+        }
+        else {
+        }
+      }
+
+    },
+    stage: function () {
+      console.log("Changed Stage")
+      if (this.stage == 2) {
+        if (!this.playingMusic & !this.muted) {
+          bgm.play()
+          bgm.muted = false
+          this.playingMusic = true
+        }
+
+      }
+    }
   },
   methods: {
     checkScroll: function(y){
@@ -45,7 +73,7 @@ var m = new Vue({
 
       if (y < this.headerScrollDistance) {
         this.headerScaleRatio = 1+ y / this.headerScrollDistance * (this.headerFinalScale - 1)
-        this.tvOffsetX = y / this.headerScrollDistance * this.tvFinalOffsetX
+        this.tvOffsetX = this.tvInitOffsetX + y / this.headerScrollDistance * this.tvFinalOffsetX
         this.tvOffsetY = y / this.headerScrollDistance * this.tvFinalOffsetY
       }
       else {
@@ -58,7 +86,18 @@ var m = new Vue({
     },
     swtichBgColor() {
       this.mainBgColor = bgColorList[Math.floor(Math.random() * bgColorList.length)]
-      console.log(m.mainBgColor)
+      // console.log(m.mainBgColor)
+    },
+    toggleMute() {
+      if (this.muted) {
+        this.muted = false
+        bgm.muted = false
+      }
+      else { //没有静音的状态
+        this.muted = true
+        bgm.muted = true
+      }
+
     }
 
   }
@@ -70,30 +109,40 @@ var m = new Vue({
 var bgColorList = ['7c0808','8a9eae','b96605']
 
 function getSize(){
+  console.log(m.tvInitOffsetX)
   m.windowWidth = window.innerWidth
   m.windowHeight = window.innerHeight
-  if (window.innerWidth>window.innerHeight) {
+  if (window.innerWidth>window.innerHeight) { //横屏
+    m.landscape = true
+    m.tvInitOffsetX = window.innerWidth / 2 - 147
+    m.tvFinalOffsetX = window.innerWidth * -0.05
+    console.log('TV Init Offset X: ' + m.tvInitOffsetX)
     m.headerFinalScale = window.innerWidth / ( window.innerHeight * m.smallScreenHPercent / m.smallScreenH * m.smallScreenW)
   }
-  else {
+  else { //竖屏
+    m.landscape = false
     m.headerFinalScale = 1 / m.smallScreenHPercent
+    m.tvInitOffsetX = 0
+    m.tvFinalOffsetX = window.innerWidth * -0.05 //屏幕中心位置在 TV 图片左 45% 处
   }
-  console.log(m.headerFinalScale)
+  console.log(m.tvInitOffsetX)
+  console.log(m.tvFinalOffsetX)
 }
 
+console.log("Let's start!")
+getSize()
+
 window.onload = function(){
-  getSize()
   var ttt = setInterval(m.swtichBgColor, 2000);
 }
 
 window.onresize = function(){
   getSize()
-  console.log("★")
+  console.log("Resized")
   // var corners = document.querySelectorAll(".x-corner")
 }
 
 window.onscroll = function(){
-
   m.checkScroll(window.scrollY)
   m.scrollY = window.scrollY
 }
@@ -101,4 +150,36 @@ window.onscroll = function(){
 function swtichBgColor() {
   m.mainBgColor = bgColorList[Math.floor(Math.random() * bgColorList.length)]
   console.log(m.mainBgColor)
+}
+
+function audioAutoPlay(id){
+  var audio = document.getElementById(id),
+    play = function(){
+      audio.play()
+      document.removeEventListener("touchstart",play, false)
+    }
+  audio.play()
+  document.addEventListener("WeixinJSBridgeReady", function () {
+    play()
+  }, false)
+  document.addEventListener('YixinJSBridgeReady', function() {
+    play()
+  }, false)
+  document.addEventListener("touchstart",play, false)
+}
+
+function stopAudio(id){
+  var audio = document.getElementById(id),
+    pause = function(){
+      audio.pause()
+      document.removeEventListener("touchstart",pause, false)
+    }
+  audio.pause()
+  document.addEventListener("WeixinJSBridgeReady", function () {
+    pause()
+  }, false)
+  document.addEventListener('YixinJSBridgeReady', function() {
+    pause()
+  }, false)
+  document.addEventListener("touchstart",pause, false)
 }
